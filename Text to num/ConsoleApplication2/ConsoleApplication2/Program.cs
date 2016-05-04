4853b7098075a2d6b3dfbb0acb547c904ff37a02
tsvetan <tsdimo@gmail.com>
2016-05-04 23:45:24 +0300

    Maximal sequence

---
+++ Text to num/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is the number I'm searching for a match in
+            uint binaryTicket = uint.Parse(Console.ReadLine());
+            //This is the pattern I'm trying to match (101)
+            uint binaryPerforator = uint.Parse(Console.ReadLine()); ;
+
+            var numBinaryDigits = Math.Ceiling(Math.Log(binaryTicket, 2));
+            for (var i = 0; i < numBinaryDigits; i++)
+            {
+                var perforatorShifted = binaryPerforator << i;
+                if ((binaryTicket & perforatorShifted) == perforatorShifted)
+                {
+                    //Imagine we have the case:
+
+                    //Ticket:
+                    //111010101
+                    //Perforator:
+                    //000000101
+
+                    //Is a match. What binary operation can we do to 0-out the final 101?
+                    //We need to AND it with 
+                    //111111010
+
+                    //To get that value, we need to invert the perforatorShifted
+                    //000000101
+                    //XOR
+                    //111111111
+                    //EQUALS
+                    //111111010
+
+                    //Which would yield:
+                    //111010101
+                    //AND
+                    //111110000
+                    //Equals
+                    //111010000
+
+                    var flipped = perforatorShifted ^ ((uint)0xFFFFFFFF);
+                    binaryTicket = binaryTicket & flipped;
+                }
+            }
+
+            string binaryTicket01 = Convert.ToString(binaryTicket, 2);
+            Console.WriteLine(binaryTicket01);
+        }
+    }
+}
---
+++ maxSeq_array/maxSeq_array.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            int length = int.Parse(Console.ReadLine());
+            int[] array = new int[length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            int maxSeq = 1;
+            int currSeq = 1;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] == array[i + 1])
+                {
+                    currSeq++;
+                }
+                else
+                {
+                    currSeq = 1;
+                }
+
+                if (currSeq > maxSeq)
+                {
+                    maxSeq = currSeq;
+                }
+            }
+            Console.WriteLine(maxSeq);
+        }
+    }
+}
