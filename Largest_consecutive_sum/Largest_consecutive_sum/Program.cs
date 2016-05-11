6933da019729514e7a5decaed29ead3d38c04583
tsvetan <tsdimo@gmail.com>
2016-05-11 12:07:16 +0300

    Largest subsum

---
+++ Largest_consecutive_sum/Largest_consecutive_sum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest_consecutive_sum
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int index = 0; index < n; index++)
+            {
+                arr[index] = int.Parse(Console.ReadLine());
+            }
+            int sum = 0;
+            int maxSum = arr[0], currentSum = arr[0];
+            int start = 0, startTemp = 0, end = 0;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                currentSum += arr[i];
+                if (currentSum < arr[i])
+                {
+                    currentSum = arr[i];
+                    startTemp = i;
+                }
+                if (currentSum > maxSum)
+                {
+                    maxSum = currentSum;
+                    start = startTemp;
+                    end = i;
+                }
+            }
+
+            for (int i = start; i <= end; i++)
+            {
+                sum += arr[i];
+            }
+            Console.WriteLine(sum);
+
+        }  
+    }
+}
