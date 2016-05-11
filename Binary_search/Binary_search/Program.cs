c6a8011cb5ad7b5294e7a76b367ddce73e25e776
tsvetan <tsdimo@gmail.com>
2016-05-11 21:06:45 +0300

    3 more from arras No.1

---
+++ Binary_search/Binary_search/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_search
+{
+    class Program
+    {
+
+        // Build a Sieve of Eratosthenes.
+        static void Main()
+        {
+            
+
+        }
+    }
+}
---
+++ Eratosthenes/Eratosthenes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eratosthenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Total = int.Parse(Console.ReadLine());
+            int cur = 1;
+            var pc = Enumerable.Range(2, Total).ToList();
+
+            while (cur <= Math.Sqrt(Total))
+            {
+                cur = pc.First(i => i > cur);
+                pc.RemoveAll(i => i != cur && i % cur == 0);
+            }
+
+            Console.WriteLine(pc.Max());
+        }
+    }
+}
