bc637e02b1a51303186d93601f38fb189f7950b9
dimoff <tsdimo@gmail.com>
2016-05-13 23:50:49 +0300

    Find the biggest sum of a subarray

---
+++ Kadane.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kadane_s_algo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 
+                2, 3, -6, -1, 2, -1, 6, 4, -8, 8
+            };
+
+            int currentSum = 0;
+            int maxSubarraySum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                //if next element is -, then set the sum back to 0
+                currentSum = Math.Max(0, currentSum + arr[i]);
+                //check if this is the maximum we have
+                maxSubarraySum = Math.Max(currentSum, maxSubarraySum);
+            }
+
+            Console.WriteLine(maxSubarraySum);
+        }
+    }
+}
